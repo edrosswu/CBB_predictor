45694323c9ab37a413e2d0f7f59ef501b6e85ee9
edrosswu <89177794+edrosswu@users.noreply.github.com>
2025-01-10 16:28:32 -0500

    Update basketballapp.R

--- basketballapp.R
+++ basketballapp.R
@@ -4,20 +4,23 @@ library(shinydashboard)
 library(rvest)
 library(stringr)
 library(scrutiny)
+library(DT)
+library(shinyWidgets)
+library(reactable)
 
-getdata <- function(x){
+getsched <- function(x){
   today <- Sys.Date()
   today <- gsub("-", "", today)
   espn <- "https://www.espn.com/mens-college-basketball/schedule/_/date/"
   final <- paste(espn, today, sep="")
-  myurl <- read_html(final) # read our webpage as html
+  myurl <- read_html(final) # read webpage as html
   myurl <- html_table(myurl)  # convert to an html table for ease of use
   
   to.parse <- myurl[[1]]  # pull the first item in the list
   df <- as.data.frame(to.parse)
   colnames(df)[1] <- "Away"
   colnames(df)[2] <- "Home_Temp"
-  mydata <- df[-c(3:5)]
+  mydata <- df[-c(4:5)]
   home_split <- str_split_fixed(mydata$Home_Temp, " ", 2)
   home_split <- substr(home_split[,2], 2, nchar(home_split[,2]))
   home_split <- gsub("^\\d+\\s+", "", home_split)
@@ -31,11 +34,13 @@ getdata <- function(x){
   # Remove original ODDS BY column
   mydata$`ODDS BY` <- NULL
   mydata$`Home_Temp` <- NULL
-  df2 <- mydata[,c(1,3,2,4,5)]
+  df2 <- mydata[,c(1,4,2,3,5,6)]
+  colnames(df2)[3] <- "Time"
+  colnames(df2)[4] <- "Location"
   df2
 }
 
-getdata()
+getsched()
 
 getbart <- function(x){
   barturl <- read_html("https://barttorvik.com/trank.php#")
@@ -51,3 +56,43 @@ getbart <- function(x){
 }
 getbart()
 
+ui <- dashboardPage(skin = "blue",
+                    dashboardHeader(title = "CBB Score Predictor"),
+                    dashboardSidebar(
+                      sidebarMenu(
+                        #used to build out pages
+                        menuItem("Home Page", tabName = "dashboard", icon = icon("home")),
+                        menuItem("Schedule", tabName = "games", icon = icon("basketball")),
+                        menuItem("Barttorvik", tabName = "bart", icon = icon("chart-line")),
+                        menuItem("About Us", tabName = "about", icon = icon("person"))
+                      )
+                    ),
+                    dashboardBody(
+                      tabItems(
+                        #each page has its own page and links to the visuals within the page
+                        tabItem(tabName = "games",
+                                fluidRow(
+                                  column(12, 
+                                         tags$h2("Today's Games", style = "text-align: center;")
+                                  )
+                                ),
+                                fluidRow(
+                                  column(12, reactableOutput("schedule_plot"), style = "padding-top: 50px;")
+                          
+                        )
+                      )
+                    )
+                    )
+) 
+
+server <- function(input, output, session) {
+  #graphs and such go here, so like visuals and that kind of thing
+  output$schedule_plot <- renderReactable({
+    reactable(
+      getsched()
+    ) 
+  })
+}
+
+
+shinyApp(ui = ui, server = server)
